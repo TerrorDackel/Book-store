e4cc1748c5c9988f2250b8faafe4008ff8f682fa
Jennifer Thomas <jenny-codes.007@gmail.com>
2024-12-18 16:07:32 +0100

    deleted error  book json

--- script.js
+++ script.js
@@ -1,47 +1,4 @@
 
-let books = {
-  { "bookOne": 
-           {
-           "name": "Die Geheimnisse des Ozeans",
-           "author": "Clara Meer",
-           "likes": 1250,
-           "liked": true,
-           "price": 19.99,
-           "publishedYear": 2018,
-           "genre": "Fantasy",
-           "comments": 
-               {
-               "name": "Leser123",
-               "comment": "Ein faszinierendes Abenteuerbuch, das mich von der ersten Seite an gefesselt hat."
-               },
-               {
-               "name": "Bookworm84",
-               "comment": "Eine romantische Geschichte, die mein Herz berührt und mich zum Nachdenken gebracht hat."
-               },
-               {
-               "name": "FantasyFanatic",
-               "comment": "Eine spannende Fantasiewelt, die ich nur schwer aus der Hand legen konnte."
-               },
-               {
-               "name": "SciFiGuru",
-               "comment": "Ein cleverer Science-Fiction-Roman mit interessanten Zeitreise-Konzepten und Charakteren."
-               },
-               {
-               "name": "NovelLover",
-               "comment": "Ein Buch, das voller magischer Überraschungen steckt und mich begeistert hat."
-               }
-           } 
-    }
-
-   } 
-
-
-
-
-
-
-
-
 // unklar hilfe video machen 
 
 // mit json datei? array da  drin function in js aufrufen lassen und dann verwenden?
